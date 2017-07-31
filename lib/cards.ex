3ad6eb677d79b203e0e88abfb3b77311868e9bde
Tommy Klumker <tommy.klumker@gmail.com>
2017-07-31 03:13:56 -0500

    function cleanup

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,27 +1,21 @@
 defmodule Cards do
 
   def create_deck do
-    
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
-
   end
 
-  def shuffle(deck) do
-    Enum.shuffle(deck)
-  end
+  def shuffle(deck), do: Enum.shuffle(deck)
 
-  def deal(deck, hand_size) do
-    Enum.split(deck, hand_size)
-  end
+  def create_hand(hand_size), do: Cards.create_deck |> Cards.shuffle |> Cards.deal(hand_size)
+  
+  def deal(deck, hand_size), do: Enum.split(deck, hand_size)
 
-  def contains?(deck, card) do
-    Enum.member?(deck, card)
-  end
+  def contains?(deck, card), do: Enum.member?(deck, card)
 
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
@@ -33,14 +27,6 @@ defmodule Cards do
       {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, _reason} -> "That file does not exist"
     end
-  end
-
-  def create_hand(hand_size) do
-    
-    Cards.create_deck 
-    |> Cards.shuffle
-    |> Cards.deal(hand_size)
-
   end
 
 end
